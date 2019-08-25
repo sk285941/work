8f02557d04ba3efeb267ec6eec6c9252d2852ec2
sk285941 <48232325+sk285941@users.noreply.github.com>
2019-08-25 13:00:43 +0800

    Add files via upload

---
+++ search.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="search.aspx.cs" Inherits="search" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:AccessDataSource ID="AccessDataSource1" runat="server" 
+            DataFile="~/App_Data/Database3.mdb" 
+            
+            SelectCommand="SELECT a.LastName AS 員工, Sum(b.Freight) AS 總計 FROM Employees AS a, Orders AS b WHERE (((a.LastName)= 'Fuller' ) AND ((b.EmployeeID)=[a].[EmployeeID]) AND ((b.[OrderDate]) Between # 1/1/1996 # And # 1/1/1997 #)) GROUP BY a.LastName, a.EmployeeID "></asp:AccessDataSource>
+        
+        
+        <asp:GridView ID="GridView1" runat="server" DataSourceID="AccessDataSource1">
+        </asp:GridView>
+        
+        
+    </div>
+    </form>
+</body>
+</html>
---
+++ search.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class search : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        String name = Request.QueryString["name"];
+        String date1 = Request.QueryString["date1"];
+        String date2 = Request.QueryString["date2"];
+        String choice = Request.QueryString["choice"];
+
+        if (choice == "1") 
+        {
+            //物
+            AccessDataSource1.SelectCommand =
+                "SELECT c.ShipCountry, Sum(a.UnitPrice) AS 總計" +
+                "FROM [Order Details] AS a, Products AS b , Orders AS c" +
+                "WHERE b.ProductName = '" + name + "' AND b.ProductID = a.ProductID AND c.OrderDate Between #" + date1 + "# And #" + date2 + "#" +
+                "GROUP BY  c.ShipCountry" +
+                "ORDER BY Sum(a.UnitPrice) DESC";
+        }
+        else if (choice == "2")
+        {
+            //地
+            AccessDataSource1.SelectCommand =
+                "SELECT distinct  b.ProductName, Sum(a.UnitPrice) AS 總計" +
+                "FROM [Order Details] AS a, Products AS b , Orders AS c, Categories AS d" +
+                "WHERE c.ShipCountry = '" + name + "' AND c.OrderID = a.OrderID AND c.OrderDate Between #" + date1 + "# And #" + date2 + "# AND a.ProductID = b.ProductID" +
+                "GROUP BY  c.ShipCountry, b.ProductName" +
+                "ORDER BY Sum(a.UnitPrice) DESC";
+        }
+        else if (choice == "3")
+        {
+            //時
+            AccessDataSource1.SelectCommand =
+                "SELECT Sum(a.Freight) AS 總計" +
+                "FROM Orders AS a" +
+                "WHERE (((a.OrderDate) Between #" + date1 + "# And #" + date2 + "#))";
+        }
+        else if (choice == "4")
+        {
+            //事
+            AccessDataSource1.SelectCommand =
+                "SELECT b.CompanyName, a.ProductName, a.QuantityPerUnit, a.UnitPrice" +
+                "FROM Products AS a, Suppliers AS b" +
+                "WHERE b.CompanyName LIKE '" + name + "' AND b.SupplierID = a.SupplierID";
+        }
+        else if (choice == "5")
+        {
+            //人
+            AccessDataSource1.SelectCommand = 
+                "SELECT a.LastName AS 員工, Sum(b.Freight) AS 總計 FROM Employees AS a, Orders AS b "+
+                "WHERE (((a.LastName)='" + name + "') AND ((b.EmployeeID)=[a].[EmployeeID]) AND ((b.[OrderDate]) Between #" + date1 + "# And #" + date2 + "#)) "+
+                "GROUP BY a.LastName, a.EmployeeID";
+        }
+        
+        
+        
+        
+        //時 SELECT Sum(a.Freight) AS 總計 FROM Orders AS a WHERE (((a.OrderDate) Between #7/1/1996# And #7/31/1996#))
+        /*AccessDataSource1.SelectCommand = 
+            "SELECT Sum(a.Freight) AS 總計 " + 
+            "FROM Orders AS a " +
+            "WHERE (((a.OrderDate) Between #" + date1 + "# And #" + date2 + "#));";*/
+       
+
+
+    }
+}
